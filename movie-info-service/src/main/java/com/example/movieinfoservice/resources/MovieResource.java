15f84f3950d933d8553f7dd33bc65db87c83be76
mo-ayman <m.ayman.saaed@gmail.com>
2024-03-10 13:04:20 +0200

    update movie info service to use mongodb as cache

--- movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -2,32 +2,28 @@ package com.example.movieinfoservice.resources;
 
 import com.example.movieinfoservice.models.Movie;
 import com.example.movieinfoservice.models.MovieSummary;
+import com.example.movieinfoservice.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 
-    @Value("${api.key}")
-    private String apiKey;
 
-    private RestTemplate restTemplate;
+    private final MovieService movieService;
 
-    public MovieResource(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public MovieResource(MovieService movieService) {
+        this.movieService = movieService;
     }
-
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        // Get the movie info from TMDB
-        final String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
-        MovieSummary movieSummary = restTemplate.getForObject(url, MovieSummary.class);
 
-        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        return movieService.getMovieInfo(movieId);
     }
 }
+
---
+++ movie-info-service/src/main/java/com/example/movieinfoservice/services/MovieService.java
@@ -0,0 +1,60 @@
+package com.example.movieinfoservice.services;
+
+import com.example.movieinfoservice.dao.MovieRepository;
+import com.example.movieinfoservice.models.Movie;
+import com.example.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MovieService {
+
+    @Value("${api.key}")
+    private String apiKey;
+    private final MongoTemplate mongoTemplate;
+    private final RestTemplate restTemplate;
+
+    public MovieService(MovieRepository movieRepository, MongoTemplate mongoTemplate, RestTemplate restTemplate) {
+        this.mongoTemplate = mongoTemplate;
+        this.restTemplate = restTemplate;
+
+    }
+
+    public Movie getMovieInfo(String movieId) {
+        // Get the movie info from TMDB
+
+        MovieSummary movieSummary = getFromDB(movieId);
+        if (movieSummary == null) {
+            movieSummary = getFromAPI(movieId);
+            saveToDB(movieSummary);
+        }
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+    }
+
+    private MovieSummary getFromAPI(String movieId) {
+        final String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
+        return restTemplate.getForObject(url, MovieSummary.class);
+    }
+
+    private void saveToDB(MovieSummary movieSummary) {
+        mongoTemplate.save(movieSummary);
+    }
+
+    private MovieSummary getFromDB(String movieId) {
+        return mongoTemplate.findById(movieId, MovieSummary.class);
+    }
+
+
+}
+
+
